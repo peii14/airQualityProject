3b96912625d8afdc342f6e38c0e3f7ea784662b8
peii14 <gkoma444@gmail.com>
2021-12-09 11:38:00 +0100

    Refractor

--- cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/pei/Documents/code/cpp/airQualityProject
-# Build directory: /home/pei/Documents/code/cpp/airQualityProject/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
